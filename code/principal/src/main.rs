37138e8ef0ee007fb6687a11a7c6b079e7a512c6
flyq <flyq951@gmail.com>
2022-03-08 15:49:20 +0800

    restruct

---
+++ code/principal/src/main.rs
@@ -0,0 +1,10 @@
+use candid::Principal;
+
+fn main() {
+    let a = Principal::from_text("6qkcf-dx3kf-uy5c5-ohlq2-hrljo-a4xqz-kbpsi-h3p72-gmr4t-usdws-vqe").unwrap();
+    let b = Principal::from_text("7de4h-k5kdt-mkuaz-r7gcf-iumnb-4unbn-3nzq2-smemt-uqtnl-eamtp-pae").unwrap();
+    let c = Principal::from_text("e6arx-mlkkz-qnxnm-oedka-xws2o-re4ov-bhtgc-jhjp3-riog5-qh6yf-rae").unwrap();
+    let d = Principal::from_text("yhy6j-huy54-mkzda-m26hc-yklb3-dzz4l-i2ykq-kr7tx-dhxyf-v2c2g-tae").unwrap();
+
+    println!("{:?}\n{:?}\n{:?}\n{:?}", a.as_slice().len(), b.as_slice().len(), c.as_slice().len(), d.as_slice().len());
+}
