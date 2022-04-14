4cced9f4823d2d16eb47b85354ba4684b1529c1a
flyq <flyq951@gmail.com>
2022-04-14 08:24:47 +0800

    Update main.rs

--- code/principal/src/main.rs
+++ code/principal/src/main.rs
@@ -6,5 +6,11 @@ fn main() {
     let c = Principal::from_text("e6arx-mlkkz-qnxnm-oedka-xws2o-re4ov-bhtgc-jhjp3-riog5-qh6yf-rae").unwrap();
     let d = Principal::from_text("yhy6j-huy54-mkzda-m26hc-yklb3-dzz4l-i2ykq-kr7tx-dhxyf-v2c2g-tae").unwrap();
 
-    println!("{:?}\n{:?}\n{:?}\n{:?}", a.as_slice().len(), b.as_slice().len(), c.as_slice().len(), d.as_slice().len());
+    let can = Principal::from_text("wzsds-oiaaa-aaaal-aak2q-cai").unwrap();
+    // println!("{:?}\n{:?}\n{:?}\n{:?}", a.as_slice().len(), b.as_slice().len(), c.as_slice().len(), d.as_slice().len());
+    println!("{:?}", d.as_slice());
+    println!("{:?}", can.as_slice());
+
+    let u = u64::from_be_bytes(can.as_slice()[..8].try_into().unwrap());
+    println!("{:?}",  u);
 }
